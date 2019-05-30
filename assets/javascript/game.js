25da06fee08c6d9d46127620978c8f1224f54c54
sylvo <49357334+sjonon@users.noreply.github.com>
2019-05-30 00:14:41 -0500

    updated css sheets and wrote pseudocode for html and js files.

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+<script>
+
+//Variables
+Array containing names for the guessing game
+
+Random word from the array is selected
+on key press if the key matches a letter in the word display letter (are we hiding these letters using CSS and then unhiding them??)
+    if not then display letter under letters guessed and decrease guesses remaining count
+
+if word is guessed correctly (all letters are unhidden)
+then change picture, update win count and play sound
+
+
+
+
+
+
+</script>
