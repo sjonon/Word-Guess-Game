6fed6188509eb4af0a6d7c6dbce8587eec861ed9
sylvo <49357334+sjonon@users.noreply.github.com>
2019-05-30 23:11:20 -0500

    added html, javascript, and css to files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,46 @@
-<script>
+//variables
+var directions = document.getElementById("directions");
+var score = document.getElementById("score");
+var word = document.getElementById("word");
+var guesses = document.getElementById("guesses");
+var lettersGuessed = document.getElementById("lettersGuessed");
 
-//Variables
-Array containing names for the guessing game
+var names = ["hermione", "dumbledore", "harry", "ron", "fred", "george", "sirius", "luna", "neville", "voldemort", "dudley", "draco", "hedwig"];
+//computer chooses random word from names array
+var wordPicked = names[Math.floor(Math.random() * names.length)];
+//computer displays the word with letters hidden
+var hiddenWord = []
+for (var i = 0; i < wordPicked.length; i++){hiddenWord[i] = "_";}
 
-Random word from the array is selected
-on key press if the key matches a letter in the word display letter (are we hiding these letters using CSS and then unhiding them??)
-    if not then display letter under letters guessed and decrease guesses remaining count
-
-if word is guessed correctly (all letters are unhidden)
-then change picture, update win count and play sound
-
-
-
+var guessesRemaining = 15;
+var letters = [];
+var userScore = 0
 
 
 
-</script>
+//begin script for start of game - these may need to be broken out differently to make the game more responsive
+document.onkeypress = function (event) {
+    var userGuess = event.key;
+    console.log(wordPicked);
+    directions.textContent = "";
+    word.textContent = hiddenWord;
+    for (var i = 0; i < wordPicked.length; i++){
+        if (wordPicked.indexOf(userGuess) != -1 && wordPicked[i] === userGuess){
+                hiddenWord[i] = wordPicked[i];}
+            // the else statement here seems to fire every time rather than only when the if statement is false
+            //else {
+            //     console.log("letter not in word");
+            //     }
+        }
+    if (wordPicked.indexOf(userGuess) === -1){
+        letters.push(userGuess);
+        guessesRemaining--;
+    }
+    if (hiddenWord.indexOf("_") === -1) {
+        userScore++;
+      } else if (guessesRemaining === 0) {
+        console.log("they lose");
+      }
+    score.textContent = ("Score: " + userScore);
+    lettersGuessed.textContent = ("Letters Guessed: " + letters);
+    guesses.textContent = ("Guesses Remaining: " + guessesRemaining);}
